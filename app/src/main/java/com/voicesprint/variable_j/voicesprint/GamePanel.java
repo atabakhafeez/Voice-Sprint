fb88683929a18eef7fe75cd8ba0dc53e82d72f93
Atabak Hafeez <atabakh@Atabaks-MacBook-Pro.local>
2016-04-16 15:56:24 +0200

    Initial commit

---
+++ app/src/main/java/com/voicesprint/variable_j/voicesprint/GamePanel.java
@@ -0,0 +1,64 @@
+package com.voicesprint.variable_j.voicesprint;
+
+
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    private MainThread thread;
+    public GamePanel(Context context)
+    {
+        super(context);
+
+
+        //add the callback to the surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+        while(retry)
+        {
+            try{thread.setRunning(false);
+                thread.join();
+
+            }catch(InterruptedException e){e.printStackTrace();}
+            retry = false;
+        }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+
+        //we can safely start the game loop
+        thread.setRunning(true);
+        thread.start();
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+    public void update()
+    {
+
+
+
+    }
+}
