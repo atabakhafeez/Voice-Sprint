f527842255679e91f8fe0b602a23633988138292
asbhattara <as.bhattarai@jacobs-university.de>
2016-04-17 08:38:43 +0200

    Create Audio class

---
+++ app/src/main/java/com/voicesprint/variable_j/voicesprint/Audio.java
@@ -0,0 +1,83 @@
+package com.voicesprint.variable_j.voicesprint;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by ashmin on 17/04/16.
+ */
+public class Audio
+{
+    private int audioSource = MediaRecorder.AudioSource.MIC;
+    private int samplingRate = 8000; /* in Hz*/
+    private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int bufferSize = AudioRecord.getMinBufferSize(samplingRate, channelConfig, audioFormat);
+    private int sampleNumBits = 16;
+    private int numChannels = 1;
+    private boolean isRecording = false;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+
+    public void Audio()
+    {
+        recorder = new AudioRecord(audioSource, samplingRate, channelConfig, audioFormat, bufferSize);
+
+    }
+
+    private void startRecording() {
+
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                ByteBuffer bData = ByteBuffer.allocate(bufferSize);
+                byte bbarray[] = new byte[bData.remaining()];
+                bData.get(bbarray);
+                while (isRecording) {
+
+                    int result = recorder.read(bbarray, 0, bufferSize);
+                    System.out.println("READ DATA");
+                    if (result > 0) {
+                        //our algorithm
+                        System.out.println("READING");
+                    } else if (result == AudioRecord.ERROR_INVALID_OPERATION) {
+                        Log.e("Recording", "Invalid operation error");
+                        break;
+                    } else if (result == AudioRecord.ERROR_BAD_VALUE) {
+                        Log.e("Recording", "Bad value error");
+                        break;
+                    } else if (result == AudioRecord.ERROR) {
+                        Log.e("Recording", "Unknown error");
+                        break;
+                    }
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/voicesprint/variable_j/voicesprint/Superman.java
@@ -0,0 +1,7 @@
+package com.voicesprint.variable_j.voicesprint;
+
+/**
+ * Created by ashmin on 16/04/16.
+ */
+public class Superman {
+}
